31d06f3e417bdd4c88509a1d5c5eeda225dd7257
YAGI0423 <9945735@naver.com>
2025-12-29 19:22:03 +0900

    Edit README

--- hparams.py
+++ hparams.py
@@ -1,4 +1,4 @@
-DATA_DIR = 'E:/project/gitProject/translator/dataset/origin/kr'   #MFA를 수행할 데이터셋 경로
+DATA_DIR = 'E:/dataset'   #MFA를 수행할 데이터셋 경로
 RESULT_DIR = 'E:/mfa_result'    #MFA를 수행하여 기록할 최종 경로
 
 SAMPLE_RATE = 22050 #재구성시 저장할 음원의 샘플레이트
